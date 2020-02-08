839f16ac9adaf1db9bfd95ded22be540bad21ee0
Ubuntu <ubuntu@ip-172-31-22-18.us-west-2.compute.internal>
2020-02-08 06:00:34 +0000

    finalized navbar/updated some css

--- fullstack/settings.py
+++ fullstack/settings.py
@@ -154,7 +154,7 @@ AWS_S3_REGION_NAME = 'us-west-2'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-AWS_S3_CUSTOM_DOMAIN = '%s.s3-us-west-2.amazonaws.com '% AWS_STORAGE_BUCKET_NAME 
+AWS_S3_CUSTOM_DOMAIN = '%s.s3-us-west-2.amazonaws.com'% AWS_STORAGE_BUCKET_NAME 
 
 STATICFILES_LOCATION = 'static/'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
