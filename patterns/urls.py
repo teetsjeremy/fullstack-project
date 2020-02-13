89c01e0d5a92d11499446e97b91aab74c7fa1a2c
Ubuntu <ubuntu@ip-172-31-22-18.us-west-2.compute.internal>
2020-02-13 08:04:46 +0000

    unable to add to db

--- patterns/urls.py
+++ patterns/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import patterns
+from .views import index, patterns
 
 
 urlpatterns = [
-    url(r'^$', patterns, name='patterns')
+    url(r'^$', patterns, name='patterns'),
+    url(r'^index/$',index, name='index')
     ]
--- patterns/views.py
+++ patterns/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
 from .models import Patterns
 from .forms import PatternsForm
 
+
 # Create your views here.
+
+def index(request):
+    """
+    Returns index.html file
+    """
+    return render(request, 'index.html')
 
 def patterns(request):
     """
     Create a view that users can upload thier own quilt patterns
     """
-    post = get_object_or_404(Patterns)
-    patterns_form = PatternsForm(request.POST, request.FILES, instance=post)
+    
+        
     if request.method=="POST":
+        patterns_form = PatternsForm(request.POST, request.FILES)
         
         if Patterns.form.is_valid():
-            name = patterns_form.cleaned_data['name']
-            size = patterns_form.cleaned_data['size']
-            description = patterns_form.cleaned_data['description']
-            image = patterns_form.cleaned_data['image']
-            return redirect('products.html', {'products_form': products_form})
+            patterns_form.save()
+            
+            patternss = auth.authenticate(
+                name = request.POST['name'],
+                size = request.POST['size'],
+                description = request.POST['description'],
+                image = request.POST['image'])
+            return redirect('products.html')
     else:
+        patterns_form = PatternsForm
         
-        return render(request, 'patterns.html', {'patterns_form': patterns_form})
+    return render(request, 'patterns.html', {"patterns_form": patterns_form})
